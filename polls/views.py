30bf91beafd5e9c1a2aab5f316fbbd11dfc12cda
Nffish <nffish@gmail.com>
2017-10-31 09:42:44 +0800

    rebuild

--- polls/views.py
+++ polls/views.py
@@ -1,31 +1,28 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
-from django.http import Http404
+from django.views import generic
 
 from .models import Question, Choice
 
 
-# /polls/index/
-def index(request):
-    #
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # context是一个由(变量名, python对象)组成的字典
-    # 变量名对应html模板中的名称
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'lastest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published question."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
