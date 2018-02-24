faf2aa82ec5b83f1e341a801b0f3f3d3b6e9575c
nffish <nffish@gmail.com>
2018-02-24 23:22:21 +0800

    配置URL

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,6 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^index/', views.index),
-
+    url(r'^index/$', views.index),
+    url(r'^article/(?P<article_id>[0-9]+)/$', views.article_view, name='article_page'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from .models import Article
 from django.http import HttpResponse
+
+from . import models
+from .models import Article
 
 
 def index(request):
     articles = Article.objects.all()
     return render(request, 'blog/index.html', {'articles': articles})
+
+
+def article_view(request, article_id):
+    article = models.Article.objects.get(pk=article_id)
+    return render(request, 'blog/article_page.html', {'article': article})
